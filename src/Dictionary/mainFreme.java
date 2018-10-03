7195ad88a2020b080e69980f38b4006277b3be48
habac99 <havanbac99@gmail.com>
2018-10-03 18:39:42 +0700

    test

---
+++ src/Dictionary/mainFreme.java
@@ -0,0 +1,36 @@
+/*
+ * Created by JFormDesigner on Wed Oct 03 15:11:36 ICT 2018
+ */
+
+package Dictionary;
+
+import java.awt.*;
+import javax.swing.*;
+import com.jgoodies.forms.layout.*;
+
+/**
+ * @author Hà
+ */
+public class mainFreme extends JFrame {
+    public mainFreme() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Hà
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FormLayout(
+            "19*(default, $lcgap), default",
+            "2*(default, $lgap), default"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Hà
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
