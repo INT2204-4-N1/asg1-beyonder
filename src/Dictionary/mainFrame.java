e536d3069cf6746ff1bfedba7ab67e24a53a3017
habac99 <havanbac99@gmail.com>
2018-10-03 22:56:36 +0700

    Don't know what to do

--- src/Dictionary/mainFrame.java
+++ src/Dictionary/mainFrame.java
@@ -1,85 +1,9 @@
 package Dictionary;
 
-import java.awt.*;
 import javax.swing.*;
-import com.jgoodies.forms.factories.*;
-import com.jgoodies.forms.layout.*;
-import net.miginfocom.swing.*;
 
 public class mainFrame {
-
-
-
-
-    public static  void main(String[] a){
-        JFrame layout = new JFrame("dictionary");
-        layout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        layout.setVisible(true);
-        layout.setSize(500, 500);
-
-    }
-
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
-
-
-
-    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Hà
-    private JPanel dialogPane;
-    private JPanel buttonBar;
-    private JButton okButton;
-    private JButton cancelButton;
-    // JFormDesigner - End of variables declaration  //GEN-END:variables
-
-    private void initComponents() {
-        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - Hà
-        dialogPane = new JPanel();
-        buttonBar = new JPanel();
-        okButton = new JButton();
-        cancelButton = new JButton();
-
-        //======== this ========
-        Container contentPane = getContentPane();
-        contentPane.setLayout(new BorderLayout());
-
-        //======== dialogPane ========
-        {
-            dialogPane.setBorder(Borders.createEmptyBorder("7dlu, 7dlu, 7dlu, 7dlu"));
-
-            // JFormDesigner evaluation mark
-            dialogPane.setBorder(new javax.swing.border.CompoundBorder(
-                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                    java.awt.Color.red), dialogPane.getBorder())); dialogPane.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
-            dialogPane.setLayout(new BorderLayout());
-
-            //======== buttonBar ========
-            {
-                buttonBar.setBorder(Borders.createEmptyBorder("5dlu, 0dlu, 0dlu, 0dlu"));
-                buttonBar.setLayout(new FormLayout(
-                    "$glue, $button, $rgap, $button",
-                    "pref"));
-
-                //---- okButton ----
-                okButton.setText("OK");
-                buttonBar.add(okButton, CC.xy(2, 1));
-
-                //---- cancelButton ----
-                cancelButton.setText("Cancel");
-                buttonBar.add(cancelButton, CC.xy(4, 1));
-            }
-            dialogPane.add(buttonBar, BorderLayout.SOUTH);
-        }
-        contentPane.add(dialogPane, BorderLayout.CENTER);
-        pack();
-        setLocationRelativeTo(getOwner());
-        // JFormDesigner - End of component initialization  //GEN-END:initComponents
-    }
+    private JPanel panel1;
+    private JScrollBar scrollBar1;
+    private JTextField bdvgfTextField;
 }
-
