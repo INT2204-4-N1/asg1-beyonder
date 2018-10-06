ec6127b05a7a6ae94ff152f68792ee050228606c
habac99 <havanbac99@gmail.com>
2018-10-07 01:09:36 +0700

    tạch môn

--- src/EVDictionary/Controller.java
+++ src/EVDictionary/Controller.java
@@ -6,17 +6,39 @@ import EVDictionary.analyzeData;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
-public class Controller  {
-    File F = new File("E_V.txt");
-    analyzeData aD ;
+public class Controller extends Application {
+    private  ListView<String> wordView;
+    public   void showWord(ArrayList<String> arr){
 
+        ObservableList<String>  words = FXCollections.observableArrayList(arr);
+        wordView = new ListView(words);
+        StackPane root = new StackPane();
+        root.getChildren().add(wordView);
+        Scene scene = new Scene(root, 350, 200);
+        stage.setScene(scene);
+        stage.show();
 
-
-
-
-
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
--- src/EVDictionary/DictionaryLayout.java
+++ src/EVDictionary/DictionaryLayout.java
@@ -1,15 +1,22 @@
 package EVDictionary;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.util.ArrayList;
 
 public class DictionaryLayout extends Application {
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -21,6 +28,7 @@ public class DictionaryLayout extends Application {
     }
 
 
-    public static void main(String[] args) { launch(args);}
-}
+    public static void main(String[] args) {
 
+        launch(args);}
+}
